6b5f9ca300f00db2f1200d3a7a8fb3c4e381c954
Nabeel Ahmed <nabeelmoolji1@gmail.com>
2020-02-22 19:40:34 +0500

    Remove unwanted imports

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template,url_for, request, redirect
-from flask_wtf import Form
-from wtforms import validators, ValidationError
-from wtforms import TextField
 import numpy as np
 import puzzle as pz
 import AStarWithBackTrack as astar
